3284a25a4487d6addfcc5375e6620a8d98eae87d
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-08-03 20:20:38 +0300

    Create quiz4.py

---
+++ quiz4.py
@@ -0,0 +1,43 @@
+import random
+
+class QA:
+  def __init__(self, question, correctAnswer, otherAnswers):
+    self.question = question
+    self.corrAnsw = correctAnswer
+    self.otherAnsw = otherAnswers
+
+qaList = [QA("How many champinships with Chicago Bulls has Michael Jordan win?", "six", ["one", "three"]),
+QA("How many championships with Milwaukee Bucks has Giannis Antetokounmpo win?", "one", ["zero", "two", "four"]),
+QA("How many championships with Los Angeles Lakers has Kobe Bryant win?", "five", ["one", "three", "six"]),
+QA("How many Olympic gold metals has LeBron James win?", "two", ["one", "three"]),
+QA("How many Champions League titles has Lionel Messi win?", "four", ["one", "two", "three", "five"])]
+
+corrCount = 0
+random.shuffle(qaList)
+for qaItem in qaList:
+  print(qaItem.question)
+  print("Possible answers are:")
+  possible = qaItem.otherAnsw + [qaItem.corrAnsw] # square brackets turn correct answer into list for concatenating with other list
+  random.shuffle(possible)
+  count = 0 # list indexes start at 0 in python
+  while count < len(possible):
+    print(str(count+1) + ": " + possible[count])
+    count += 1
+  print("Please enter the number of your answer:")
+  userAnsw = input()
+  while not userAnsw.isdigit():
+    print("That was not a number. Please enter the number of your answer:")
+    userAnsw = input()
+  userAnsw = int(userAnsw)
+  while not (userAnsw > 0 and userAnsw <= len(possible)):
+    print("That number doesn't correspond to any answer. Please enter the number of your answer:")
+    userAnsw = input()
+  if possible[userAnsw-1] == qaItem.corrAnsw:
+    print("Your answer was correct.")
+    corrCount += 1
+  else:
+    print("Your answer was wrong.")
+    print("Correct answer was: " + qaItem.corrAnsw)
+  print("")
+
+print("You answered " + str(corrCount) + " of " + str(len(qaList)) + " questions correctly.")
